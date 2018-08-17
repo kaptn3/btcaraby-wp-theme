0624a5bd7643775b56b4a26f5413c85c5c67dff8
vikapitoshka <kapitonenko.vick@ya.ru>
2018-08-17 23:44:33 +0300

    Init theme

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <title>
+    <?php echo wp_get_document_title(); ?>
+  </title>
+
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header class="header">
+    <h1><?php bloginfo( 'name' ); ?></h1>
+    <h2><?php bloginfo( 'description' ); ?></h2>
+  </header>
+
+  <div class="middle">
+  <?php
+    if ( have_posts() ){
+      while ( have_posts() ){
+        the_post();
+
+        echo '<h3><a href="'. get_permalink() .'">'. get_the_title() .'</a></h3>';
+
+        echo get_the_excerpt();
+      }
+    }
+    // елси записей не найдено
+    else{
+      echo ' <p>Записей нет...</p>';
+    }
+    ?>
+  </div>
+
+  <footer class="footer">
+    <?php echo date('Y') ?> © btcaraby
+  </footer>
+
+  <?php wp_footer(); ?>
+</body>
+
+</html>
