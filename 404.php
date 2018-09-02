a592c464501ffe81da6d0ab69575f0e6982a6e8e
vikapitoshka <kapitonenko.vick@ya.ru>
2018-09-02 18:11:14 +0300

    404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+ <?php get_header(); ?>
+
+<div class="container">
+  <div class="row page-wrapper">
+    <div class="col-12 col-md-8">
+      <article class="article" class="<?php post_class(); ?>">
+        <h2 class="header-section">404 خطأ</h2>
+        <div class="page__content">
+          <h3>وجه الفتاة! لا يمكن العثور على هذه الصفحة</h3>
+        </div>
+      </article>
+    </div>
+    <?php get_sidebar(); ?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
